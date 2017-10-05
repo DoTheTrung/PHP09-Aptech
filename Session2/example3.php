41fee8ac90ca784d4b89907250514883f443a287
The Trung <dothetrung3196@gmai.com>
2017-10-05 18:12:40 +0700

    example3

---
+++ Session2/example3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Example3</title>
+</head>
+<body>
+	<form action="welcome.php" method="post">
+		<input type="text" name="name" placeholder="Input your name"> <br><br>
+		<input type="text" name="class" placeholder="Input your class"> <br><br>
+		<input type="text" name="birthday" placeholder="Input your birthday"> <br><br>
+		<input type="text" name="email" placeholder="Input your email address"> <br><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
